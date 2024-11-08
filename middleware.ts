46fc7d17508f215a23acc9503f8fbeaf58140435
Ibrahim <m.ibrahm.0001@gmail.com>
2024-11-08 23:54:58 +0500

    Changed middleware.ts file

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,11 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const isProtectedRoute = createRouteMatcher(["/"]);
 
-export default clerkMiddleware(async (auth, request) => {
-  console.log("Middleware triggered for URL:", request.nextUrl.pathname);
-  
-  if (!isPublicRoute(request)) {
-    console.log("Attempting to protect route:", request.nextUrl.pathname);
-    await auth.protect();
-  } else {
-    console.log("Public route accessed:", request.nextUrl.pathname);
-  }
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth.protect();
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
